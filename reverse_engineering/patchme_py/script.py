a9a1964e102d36c1e49d857f010f3381916580d3
uetsymphonique <minhtran6464.lhp@gmail.com>
2023-11-17 17:30:55 +0700

    Solve problem: patchme.py

---
+++ reverse_engineering/patchme_py/script.py
@@ -0,0 +1,13 @@
+def str_xor(secret, key):
+    new_key = key
+    i = 0
+    while len(new_key) < len(secret):
+        new_key = new_key + key[i]
+        i = (i + 1) % len(key)
+    return "".join([chr(ord(secret_c) ^ ord(new_key_c)) for (secret_c, new_key_c) in zip(secret, new_key)])
+
+
+
+flag_enc = open('flag.txt.enc', 'rb').read()
+decryption = str_xor(flag_enc.decode(), "utilitarian")
+print(decryption)
